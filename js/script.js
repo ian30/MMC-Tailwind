3158afd15aca6efda298983360fcc633ebde900c
Ian Shnayder <ian.shnayder@hyland.com>
2023-08-09 16:41:25 -0700

    updating script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 "use strict";
-const showAllCategoriesBtn = document.getElementById("cat_allCats");
+const showAllCatsBtn = document.getElementById("cat_allCats");
+const showAction = document.getElementById("cat_action");
+const showThrillers = document.getElementById("cat_thrillers");
+const showComedy = document.getElementById("cat_comedy");
+const showScifi = document.getElementById("cat_scifi");
 
 showAllCategoriesBtn.addEventListener("click", function () {
   alert("shmoop");
+});
+showAction.addEventListener("click", () => {
+  //get all movies with class "cat_action"
+  document.querySelectorAll(".action").addClass("hidden");
 });
