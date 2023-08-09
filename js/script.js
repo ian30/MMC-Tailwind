a36906b5244d07c5fff2dfad66b452d3fc5e17b0
Ian Shnayder <ian.shnayder@hyland.com>
2023-08-08 22:09:26 -0700

    fixing variable name

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-const showAllCategoriesBtn = document.getElementById("allCats");
+const showAllCategoriesBtn = document.getElementById("cat_allCats");
 
 showAllCategoriesBtn.addEventListener("click", function () {
   alert("shmoop");
