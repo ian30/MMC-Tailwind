99478d82ea1ff07e2f062750d1671aad50ca7665
Ian Shnayder <ian.shnayder@hyland.com>
2023-08-10 05:43:55 -0700

    show/hide basic function

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ const showAction = document.getElementById("cat_action");
 const showThrillers = document.getElementById("cat_thrillers");
 const showComedy = document.getElementById("cat_comedy");
 const showScifi = document.getElementById("cat_scifi");
+const showAll = document.getElementById("cat_showAll");
 const overlays = document.querySelectorAll(".overlay");
 const cover_container = document.getElementsByClassName("cover_container");
+const movie_cards = document.getElementsByClassName("movie-card");
 
 //loop thru cover container and show the movie name on hover: (the loop looks weird because the 'getElementsByClassName' is creating
 //an object called 'html collection' and that's how you loop thru that.)
@@ -16,6 +18,12 @@ Array.from(document.getElementsByClassName("cover_container")).forEach(function 
   });
   element.addEventListener("mouseleave", () => {
     overlay_child.classList.add("invisible");
+  });
+});
+
+showAll.addEventListener("click", () => {
+  Array.from(document.getElementsByClassName("movie-card")).forEach(function (element, index, array) {
+    element.classList.toggle("hidden");
   });
 });
 
