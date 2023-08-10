b8aa7fe780f7c5d8f6af5c75ac9ecf042a925878
Ian Shnayder <ian.shnayder@hyland.com>
2023-08-10 05:15:36 -0700

    lots of updates

--- js/script.js
+++ js/script.js
@@ -4,9 +4,27 @@ const showAction = document.getElementById("cat_action");
 const showThrillers = document.getElementById("cat_thrillers");
 const showComedy = document.getElementById("cat_comedy");
 const showScifi = document.getElementById("cat_scifi");
+const overlays = document.querySelectorAll(".overlay");
+const cover_container = document.getElementsByClassName("cover_container");
 
-showAllCategoriesBtn.addEventListener("click", function () {
-  alert("shmoop");
+//loop thru cover container and show the movie name on hover: (the loop looks weird because the 'getElementsByClassName' is creating
+//an object called 'html collection' and that's how you loop thru that.)
+Array.from(document.getElementsByClassName("cover_container")).forEach(function (element, index, array) {
+  const overlay_child = element.children[0];
+  element.addEventListener("mouseenter", () => {
+    overlay_child.classList.remove("invisible");
+  });
+  element.addEventListener("mouseleave", () => {
+    overlay_child.classList.add("invisible");
+  });
+});
+
+// hide all movie overlays:
+overlays.forEach((overlays) => {
+  overlays.classList.add("invisible");
+});
+showAllCatsBtn.addEventListener("click", function () {
+  //code
 });
 showAction.addEventListener("click", () => {
   //get all movies with class "cat_action"
